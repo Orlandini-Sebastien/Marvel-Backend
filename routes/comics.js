945c60746bde9477aadb3eb1b540aaae911a6706
Orlandini Sebastien <sebastien.orlandini@gmail.com>
2024-02-10 14:12:42 +0100

    connection

--- routes/comics.js
+++ routes/comics.js
@@ -14,7 +14,7 @@ router.get("/comics", async(req, res) => {
    
     const comic = req.query.comic;
 
-    const response  = await axios.get( `https://lereacteur-marvel-api.herokuapp.com/comics?title=${comic}&skip=${limit*page}&apiKey=${process.env.KEY}`)
+    const response  = await axios.get( `https://lereacteur-marvel-api.herokuapp.com/comics?title=${comic}&skip=${limit*page}&apiKey=${process.env.MARVEL_KEY}`)
     res.json(response.data.results);
     } catch (error) {
     res.json({error : error.message})
@@ -25,7 +25,7 @@ router.get("/comics", async(req, res) => {
 router.get("/comic/:characterId", async(req, res) => {
     try {
         const characterId = req.params.characterId
-      const response  = await axios.get( `https://lereacteur-marvel-api.herokuapp.com/comics/${characterId}?apiKey=${process.env.KEY}`)
+      const response  = await axios.get( `https://lereacteur-marvel-api.herokuapp.com/comics/${characterId}?apiKey=${process.env.MARVEL_KEY}`)
       res.json(response.data);
       } catch (error) {
       res.json({error : error.message})
@@ -40,7 +40,7 @@ router.get("/comic/:characterId", async(req, res) => {
     try {  
 
        const comicId = req.params.comicId;
-      const response  = await axios.get( `https://lereacteur-marvel-api.herokuapp.com/comic/${comicId}?apiKey=${process.env.KEY}`)
+      const response  = await axios.get( `https://lereacteur-marvel-api.herokuapp.com/comic/${comicId}?apiKey=${process.env.MARVEL_KEY}`)
       return res.status(201).json(response.data);
       } catch (error) {
       return res.status(400).json({error : error.message})
---
+++ routes/userMarvel.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const router = express.Router();
+const UserMarvel = require( "../models/UserMarvel");
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
+const uid2 = require("uid2");
+
+const fileUpload = require("express-fileupload");
+
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+  });
+
+const convertToBase64 = (file) => {
+return `data:${file.mimetype};base64,${file.data.toString("base64")}`;
+};
+
+router.post("/userMarvel/signup", fileUpload(), async(req,res)=>{
+    try {
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.password;
+        const newsletter = req.body.newsletter;
+
+   
+        const salt = uid2(16);
+        const hash = SHA256(password + salt).toString(encBase64);
+        const token = uid2(16);
+
+        const emailAlreadyExist = await UserMarvel.find({email : req.body.email});
+        if(emailAlreadyExist[0] !== undefined){
+            return res.status(400).json({message : "Email already exist"});
+        }
+
+        const newUser = new UserMarvel( {
+            email: email,
+            account: {
+              username: username,
+              avatar: Object, 
+            },
+            newsletter: newsletter,
+            token: token,
+            hash: hash,
+            salt: salt,
+          });
+
+        // Upload avatar to the request
+        if(req.files?.avatar) {
+            const convertedPicture = convertToBase64(req.files.avatar);
+            const uploadResult = await cloudinary.uploader.upload(
+                convertedPicture,
+                {folder: `/marvel/users/${newUser._id}`},
+                function(error, result) { console.log(result, error);}
+        );
+        
+        // Save the picture in cloudinary
+        newUser.account.avatar = uploadResult;
+        }
+
+        await newUser.save();
+        
+        res.status(200).json({
+            "_id":newUser._id,
+            "token" : newUser.token,
+            "account" : newUser.account
+        });
+    } catch (error) {
+        res.status(500).json({message : error.message});
+    }
+});     
+
+router.post("/userMarvel/login", async(req,res)=>{
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const user = await UserMarvel.findOne({email : email});
+        if(!user){
+            return res.status(401).json({message : "Email dosn't exist"});
+        }
+
+        const salt = user.salt;
+        const hash = SHA256(password + salt).toString(encBase64);
+        if(hash === user.hash){
+           res.status(200).json({
+            "_id" : user._id,
+            "token": user.token,
+            "account" : user.account
+            }); 
+        } else {
+            res.status(401).json({message : "Password do not match"})
+        }
+    } catch (error) {
+        res.status(500).json({message : error.message});
+    }
+});
+
+module.exports= router;
